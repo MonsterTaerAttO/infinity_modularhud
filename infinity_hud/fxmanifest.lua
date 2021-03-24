541cba75823e4d5d0843f2e22bfe8e122e8b05c2
1NF1N17Y20 <78461134+1NF1N17Y20@users.noreply.github.com>
2021-03-24 09:04:40 -0400

    Initial Release

---
+++ infinity_hud/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+game 'gta5'
+version '1.0.1'
+description 'nafing Hud DC: ✪ dydolek#2137'
+
+ui_page 'html/ui.html'
+
+client_scripts {
+	'client.lua',
+}
+
+server_scripts {
+	"server.lua"
+}
+
+files {
+	'html/ui.html',
+	'html/style.css',
+	'html/main.js',
+	'html/img/*.png',
+}
+
+export 'HealthEdit'
+export 'ArmorEdit'
+export 'FuelEdit'
---
+++ infinity_hud/html/main.js
@@ -0,0 +1,91 @@
+﻿window.addEventListener("message", function (event) {
+  switch (event.data.action) {
+
+    case "intialization":
+      var elemHealth = $('.mainHealth')[0];
+      var elemArmor = $('.mainArmor')[0];
+      var elemFuel = $('.hudCar')[0];
+      elemHealth.style.left = event.data.healthleft + "%";
+      elemHealth.style.bottom = event.data.healthbottom + "%";
+
+      elemArmor.style.left = event.data.armorleft + "%";
+      elemArmor.style.bottom = event.data.armorbottom + "%";
+
+      elemFuel.style.left = event.data.fuelleft + "%";
+      elemFuel.style.bottom = event.data.fuelbottom + "%";
+      break;
+
+    case "edithealth":
+      var elem = $('.mainHealth')[0];
+      elem.style.left = event.data.left + "%";
+      elem.style.bottom = event.data.bottom + "%";
+      break;
+
+    case "editarmor":
+      var elem = $('.mainArmor')[0];
+      elem.style.left = event.data.left + "%";
+      elem.style.bottom = event.data.bottom + "%";
+      break;
+
+    case "editfuel":
+      var elem = $('.hudCar')[0];
+      elem.style.left = event.data.left + "%";
+      elem.style.bottom = event.data.bottom + "%";
+      break;
+
+
+
+    case "huddis":
+      Progress(event.data.health, ".health");
+      Progress(event.data.armor, ".armor");
+      if (event.data.showhud == true) {
+        $(".mainHealth").fadeIn();
+        $(".mainArmor").fadeIn();
+        Progress(event.data.health, ".health");
+      } else {
+        $(".mainHealth").fadeOut();
+        $(".mainArmor").fadeOut();
+      }
+      break;
+
+    case "car":
+      if (event.data.showhud == true) {
+        $(".hudCar").fadeIn();
+        setProgressFuel(event.data.fuel, ".progress-fuel");
+      } else {
+        $(".hudCar").fadeOut();
+      }
+      break;
+      
+  }
+});
+
+function Progress(percent, element) {
+  var circle = document.querySelector(element);
+  var radius = circle.r.baseVal.value;
+  var circumference = radius * 2 * Math.PI;
+  var html = $(element).parent().parent().find("span");
+
+  circle.style.strokeDasharray = `${circumference} ${circumference}`;
+  circle.style.strokeDashoffset = `${circumference}`;
+
+  const offset = circumference - ((-percent * 100) / 100 / 100) * circumference;
+  circle.style.strokeDashoffset = -offset;
+
+  html.text(Math.round(percent));
+}
+
+function setProgressFuel(percent, element) {
+  var circle = document.querySelector(element);
+  var radius = circle.r.baseVal.value;
+  var circumference = radius * 2 * Math.PI;
+  var html = $(element).parent().parent().find("span");
+
+  circle.style.strokeDasharray = `${circumference} ${circumference}`;
+  circle.style.strokeDashoffset = `${circumference}`;
+
+  const offset = circumference - ((-percent * 73) / 100 / 100) * circumference;
+  circle.style.strokeDashoffset = -offset;
+
+  html.text(Math.round(percent));
+}
